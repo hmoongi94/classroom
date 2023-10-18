e9afc8f57fe731271916922177034da513db5103
HongMoonGI <hmoongi94@gmail.com>
2023-10-18 16:40:48 +0900

    root변수 제거 필요없음

--- develop-training-11_loginForm/inputValue.js
+++ develop-training-11_loginForm/inputValue.js
@@ -1,4 +1,12 @@
-const root = document.getElementById("root")
 const jsonData = require("./signUpAssetModule.js")
+const input = document.getElementsByTagName("input")
+// console.log(jsonData)
 
-console.log(jsonData)
+const {username, password1, password2, email} = jsonData
+
+username = input[0].value
+password1 = input[1].value
+password2 = input[2].value
+email = input[3].value
+
+module.exports = inputValue
