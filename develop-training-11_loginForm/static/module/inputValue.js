d635346fb516b67c361319e68793eb31a84e337c
HongMoonGI <hmoongi94@gmail.com>
2023-10-18 17:52:56 +0900

    가독성을 위해 주석지움

--- develop-training-11_loginForm/static/module/inputValue.js
+++ develop-training-11_loginForm/static/module/inputValue.js
@@ -7,6 +7,6 @@ const {id, password1, password2, email} = jsonData
 jsonData.id = input[0].value
 jsonData.password1 = input[1].value
 jsonData.password2 = input[2].value
-jsonData.email = input[0].value
+jsonData.email = input[3].value
 
 module.exports = jsonData
