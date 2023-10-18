02d44fe2a94a318818192213d803bbb58f379b62
HongMoonGI <hmoongi94@gmail.com>
2023-10-18 18:42:37 +0900

    js파일이 적용이 안되는이유 찾는중

--- develop-training-11_loginForm/static/module/inputValue.js
+++ develop-training-11_loginForm/static/module/inputValue.js
@@ -1,12 +1,10 @@
 const signUpAsset = require("./signUpAssetModule.js")
 
 const jsonData = signUpAsset
-const input = document.getElementsByTagName("input")
-const h1 = document.getElementsByTagName("h1")
 
 jsonData.id = input[0].value
 jsonData.password1 = input[1].value
 jsonData.password2 = input[2].value
 jsonData.email = input[3].value
 
-h1.textContent = `${input[0].value}님! 반갑습니다. 저에게 편지를 보내주세요.`
+module.exports = jsonData
---
+++ develop-training-11_loginForm/static/module/loginSuccess.js
@@ -0,0 +1,4 @@
+const h1 = document.getElementsByTagName("h1")
+// const input = document.getElementsByTagName("input")
+
+h1[0].textContent = "님! 반갑습니다. 저에게 편지를 보내주세요."
