2c9b2a9b983c097770e54c37f415c4c33c0fdd34
HongMoonGI <hmoongi94@gmail.com>
2023-10-19 18:31:27 +0900

    조건문 연습하기

---
+++ develop-training-11_loginForm/typeofpractice.js
@@ -0,0 +1,13 @@
+const signUpAsset = {
+  username:"",
+  password1:"",
+  password2:"",
+  email:"",
+  textColor: "#B6B6B6",
+  inputBoxColor:"#D9D9D9",
+  pointColor:"#FF6666"
+}
+
+const {username, password1, password2, email} =signUpAsset
+console.log(username)
+console.log(typeof(username))
