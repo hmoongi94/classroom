dd35f08395976d4a2a637a5262154ded5dad28e5
HongMoonGI <hmoongi94@gmail.com>
2023-10-19 11:43:52 +0900

    로그인폼 signupasset모듈 연결

--- develop-training-11_loginForm/static/module/inputValue.js
+++ develop-training-11_loginForm/static/module/inputValue.js
@@ -1,11 +1,11 @@
 const signUpAsset = require("./signUpAssetModule.js")
 
 const jsonData = signUpAsset
-
-jsonData.username = input[0].value
-jsonData.password1 = input[1].value
-jsonData.password2 = input[2].value
-jsonData.email = input[3].value
-
 console.log(jsonData)
+
+// jsonData.username = input[0].value
+// jsonData.password1 = input[1].value
+// jsonData.password2 = input[2].value
+// jsonData.email = input[3].value
+
 module.exports = jsonData
--- develop-training-11_loginForm/static/module/signUpAssetModule.js
+++ develop-training-11_loginForm/static/module/signUpAssetModule.js
@@ -1,5 +1,5 @@
 const signUpAsset = {
-  id:"",
+  username:"",
   password1:"",
   password2:"",
   email:"",
@@ -7,5 +7,18 @@ const signUpAsset = {
   textColor: "#B6B6B6",
   pointColor:"#FF6666"
 }
+
+input = document.getElementsByTagName('input')
+
+
+input[4].addEventListener("click",function(){
+  signUpAsset.username = input[0].value
+  signUpAsset.password1 = input[1].value
+  signUpAsset.password2 = input[2].value
+  signUpAsset.email = input[3].value
+  
+  console.dir(input)
+  console.log(signUpAsset)
+})
 
 module.exports = signUpAsset
