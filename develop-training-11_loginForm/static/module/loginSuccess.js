45739c2834bccc9dfc809a246d45a53ebff50b23
HongMoonGI <hmoongi94@gmail.com>
2023-10-19 12:01:29 +0900

    loginSuccess.js 비활성화

--- develop-training-11_loginForm/static/module/loginSuccess.js
+++ develop-training-11_loginForm/static/module/loginSuccess.js
@@ -1,5 +1,5 @@
-const signUpAsset = require("./signUpAssetModule");
-console.log(signUpAsset)
+// const signUpAsset = require("./signUpAssetModule");
+// console.log(signUpAsset)
 
-h1 = document.getElementsByTagName("h1")
-h1[0].textContent = `${signUpAsset.username}님! 접속을 환영합니다. 저에게 편지를 보내주세요!`
+// h1 = document.getElementsByTagName("h1")
+// h1[0].textContent = `${signUpAsset.username}님! 접속을 환영합니다. 저에게 편지를 보내주세요!`
