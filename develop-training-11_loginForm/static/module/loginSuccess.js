58da54c0907887371ddf7292e143fca813e6c01e
HongMoonGI <hmoongi94@gmail.com>
2023-10-19 11:58:29 +0900

    loginSuccess.js에 require이 안먹혀서 문제가됨

--- develop-training-11_loginForm/static/module/loginSuccess.js
+++ develop-training-11_loginForm/static/module/loginSuccess.js
@@ -1,4 +1,5 @@
 const signUpAsset = require("./signUpAssetModule");
+console.log(signUpAsset)
 
-document.getElementsByTagName("h1")
+h1 = document.getElementsByTagName("h1")
 h1[0].textContent = `${signUpAsset.username}님! 접속을 환영합니다. 저에게 편지를 보내주세요!`
