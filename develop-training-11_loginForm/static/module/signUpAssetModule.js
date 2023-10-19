03f70e3dbdfd81f38a9b4ae1a9704565d88f398a
HongMoonGI <hmoongi94@gmail.com>
2023-10-19 11:48:53 +0900

    signupasset.js loginform.html 연결

--- develop-training-11_loginForm/static/module/signUpAssetModule.js
+++ develop-training-11_loginForm/static/module/signUpAssetModule.js
@@ -3,13 +3,23 @@ const signUpAsset = {
   password1:"",
   password2:"",
   email:"",
-  inputBoxColor:"#D9D9D9",
   textColor: "#B6B6B6",
+  inputBoxColor:"#D9D9D9",
   pointColor:"#FF6666"
 }
 
 input = document.getElementsByTagName('input')
+console.dir(input)
+input[0].style.backgroundColor = signUpAsset.inputBoxColor
+input[1].style.backgroundColor = signUpAsset.inputBoxColor
+input[2].style.backgroundColor = signUpAsset.inputBoxColor
+input[3].style.backgroundColor = signUpAsset.inputBoxColor
+input[4].style.backgroundColor = signUpAsset.pointColor
 
+input[0].color = signUpAsset.textColor
+input[1].color = signUpAsset.textColor
+input[2].color = signUpAsset.textColor
+input[3].color = signUpAsset.textColor
 
 input[4].addEventListener("click",function(){
   signUpAsset.username = input[0].value
@@ -17,8 +27,8 @@ input[4].addEventListener("click",function(){
   signUpAsset.password2 = input[2].value
   signUpAsset.email = input[3].value
   
-  console.dir(input)
-  console.log(signUpAsset)
+  // console.dir(input)
+  // console.log(signUpAsset)
 })
 
 module.exports = signUpAsset
