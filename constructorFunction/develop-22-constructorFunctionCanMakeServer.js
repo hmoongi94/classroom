14dc4287cb83950b57dc0849db2aed7642e46c94
HongMoonGI <hmoongi94@gmail.com>
2023-10-30 19:24:17 +0900

    생성자 함수로 서버 만들기

--- constructorFunction/develop-22-constructorFunctionCanMakeServer.js
+++ constructorFunction/develop-22-constructorFunctionCanMakeServer.js
@@ -7,11 +7,35 @@ class SimpleServer{
   // *'인자를 받는다' 라고 표현하기도 한다.
   constructor(port){
     // 3. 여기서 작성된 this는 만들어질 인스턴스 객체를 이야기 한다.
-    this.port
+    this.port = port
   }
   // 4. 만들어질 인스턴스(객체)에 메서드로 사용될 start()함수이다.
   // * 리터럴로 작성해왔던 createServer()가 작성되어있는 것을 확인 할 수 있다.
   start(){
-    
+    const server = http.createServer((req,res)=>{
+      res.writeHead(200, {'Content-Type':'text/plain'})
+      res.end("생성자 함수로 가동된 서버입니다.")
+    });
+    // 5. this.port는 생성자 함수에서 받은 파라미터 값이다.
+    // * 'port' 정보는, 추후, 생성될 때 결정되기 때문에 인자를 전달받은 형태이다.
+    server.listen(this.port,()=>{
+      console.log(`http://localhost:${this.port}`)
+    })    
   }
+}
+
+// SimpleServer 인스턴스 생성 및 시작
+const simpleApp = new SimpleServer(3000);
+simpleApp.start();
+
+const portRange = {
+  min:3001,
+  max:3005
+}
+
+// 3001,3002,3003,3004,3005 port 모두 열렸다.
+// 각각의 역할을 하는 포트를 만들어서 사용할 수 있다.
+for(let i =portRange.min; i<portRange.max; i++){
+  const app = new SimpleServer(i);
+  app.start();
 }
