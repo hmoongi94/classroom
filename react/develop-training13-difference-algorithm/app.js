7a623dc8198739b3763ad64d8119a65d3b10b3eb
HongMoonGI <hmoongi94@gmail.com>
2023-11-07 16:38:25 +0900

    기본 틀 작성-> 로직짜야됨

--- react/develop-training13-difference-algorithm/app.js
+++ react/develop-training13-difference-algorithm/app.js
@@ -1,2 +1,14 @@
+// custom modules
 import diffLogic from './model/diff-logic.js'
 diffLogic()
+
+// built-in modules
+import fs from "fs"
+
+// json files path
+const inputJSONPath = "./data/fromDB-data.json"
+const outputJSONPath = "./data/differences.json"
+
+// controller
+const resultObject = diffLogic(inputJSONPath, outputJSONPath)
+console.log(resultObject)
