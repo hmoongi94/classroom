6d6d32f2771ac5d01e6e1e4afb5a60f9593fda3e
HongMoonGI <hmoongi94@gmail.com>
2023-11-14 09:37:10 +0900

    프로젝트를 시작하기전 설계하기

---
+++ repeated-learning/repeat2/todoPlan.js
@@ -0,0 +1,22 @@
+/**
+ * todo: webserver 제작
+ * pseudo1: express.js를 활용한다.
+ * pseudo2: GET요청에 대응한다.
+ * *문서를 조립하는 route.js 구조
+ * *예상되는 요청 "/"
+ * 
+ * pseudo3. POST요청에 대응한다.
+ * * action = "/mainPromptInputData"
+ * * method = "POST"
+ */
+
+
+/** 
+ * todo: 찾아볼 기술
+ * pseudo4: express.js
+ * *express.js의 기본적인 사용법
+ * *express static 기능
+ * *express router 기능
+ * *express middleware 기능
+ * *찾아보면서 추가하기
+ */
