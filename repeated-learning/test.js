d9dfb6f609d1d6a821a1a393a5a936a2920b0336
HongMoonGI <hmoongi94@gmail.com>
2023-11-15 20:48:16 +0900

    로직 이해하는 테스트중

--- repeated-learning/test.js
+++ repeated-learning/test.js
@@ -3,3 +3,11 @@ let b = [4,5,6]
 a.push(b[1])
 console.log(a)
 console.log(b)
+
+let acc = []
+acc.push([])
+console.log(acc)
+console.log(acc.length)
+acc.push([])
+console.log(acc)
+console.log(acc.length)
