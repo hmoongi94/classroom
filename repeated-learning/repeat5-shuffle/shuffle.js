7450070641c9d6edde93ece5c0a1b2fae07ff152
HongMoonGI <hmoongi94@gmail.com>
2023-11-15 20:29:05 +0900

    배열 섞어주는 로직임

---
+++ repeated-learning/repeat5-shuffle/shuffle.js
@@ -0,0 +1,28 @@
+/**
+ * 주어진 배열을 무작위로 섞는 함수.
+ * @param {Array} array - 섞을 배열.
+ * @returns {Array} 무작위로 섞인 배열.
+ */
+
+function shuffleArray(array){
+  /**
+   * 주어진 최대값 내에서 무작위 인덱스를 반환하는 함수.
+   * @param {number} max - 무작위 인덱스를 생성할 때 고려할 최대범위.
+   * @returns {number} 생성된 무작위 인덱스.
+   */
+  function getRandomIndex(max){
+    return Math.floor(Math.random()*max); // 0과 max사이의 랜덤한 정수 변환
+  }
+
+  // array의 모든 요소에 대해 루프를 돌며 무작위 위치와 교환
+  array.forEach((currentValue, index)=>{
+    const randomIndex = getRandomIndex(array.length); //랜덤 인덱스 생성
+
+    // 요소 교환(swap): 배열 내의 두 요소의 위치를 교환
+    const temp = array[index]; //현재 요소 임시 저장
+    array[index] = array[randomIndex] //현재 위치에 랜덤위치의 요소 저장
+    array[randomIndex] = temp; // 랜덤 위치에 임시 저장한 요소 저장
+  })
+
+  return array; // 섞인 배열 반환
+}
