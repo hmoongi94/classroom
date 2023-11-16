41159c6228351ae839467cb67be263193622033e
HongMoonGI <hmoongi94@gmail.com>
2023-11-16 19:20:11 +0900

    menu, content의 객체값을 배열로 받는다. [{menu}, {content}]

--- react/develop-training14-state-virtualDom/js/state-virtualDom.js
+++ react/develop-training14-state-virtualDom/js/state-virtualDom.js
@@ -21,11 +21,13 @@ export function load() {
         const item = stateData[i]
         const menuItem = createElement('li', {}, createElement('a', { href: item.hash }, item.text))
         menuItems.push(menuItem)
-        // menuItems라는 배열에 sateData의 데이터값을 사용해 {type:'li', property:{}, children:{type:'a', property:{href:#data},children:data }} 값인 
+        // menuItems라는 배열에 sateData의 데이터값을 사용해 {type:'li', property:{}, children:{type:'a', property:{href:#data}, children: [data] }} 값인 
       }
       console.log(menuItems)
       const menu = createElement('ul', {}, ...menuItems)
       const content = createElement('div', {}, 'Hello React')
+      console.log(createElement('div', {}, menu, content))
+      // children: [{...},{...}]
       return createElement('div', {}, menu, content)
     }
 
