de9e9d2565a764fa552d417498f6b9abf1099151
HongMoonGI <hmoongi94@gmail.com>
2023-11-28 12:11:02 +0900

    study: 이해어느정도는 됐음. 근데 typeof virtualDom==='string' 이 부분은 아직 이해가 안감. virtualDom=객체인데?

--- react/develop-training14-state-virtualDom/js/state-virtualDom.js
+++ react/develop-training14-state-virtualDom/js/state-virtualDom.js
@@ -13,7 +13,8 @@ export function load() {
 
     //* React에서의 createElement는 React 요소를 생성하는 데 사용되며, 가상 DOM(Virtual DOM)을 통해 더 효율적으로 UI를 업데이트할 수 있게 합니다.
     //* React의 createElement 함수는 JSX(JavaScript XML) 문법에서 사용됩니다. JSX는 JavaScript의 확장 문법으로, XML과 유사한 문법을 사용하여 React 요소를 작성할 수 있게 해줍니다. JSX 코드는 Babel과 같은 트랜스파일러를 통해 일반 JavaScript 코드로 변환되어 실행됩니다.
-    
+
+    // console.log(createElement('div',{},'Hello React'))
 
     function component(stateData) {
       const menuItems = [];
@@ -33,8 +34,9 @@ export function load() {
     }
 
     function render(virtualDom) {
+      // console.log(virtualDom)
       if (typeof virtualDom === 'string') {
-        console.log(document.createTextNode(virtualDom))
+        // console.log(document.createTextNode(virtualDom))
         return document.createTextNode(virtualDom)
       }
       const element = document.createElement(virtualDom.type)
@@ -65,6 +67,7 @@ export function load() {
     const container = document.getElementById('root')
     container.appendChild(render(virtualDom))
     // console.dir(virtualDom)
+    // console.log(virtualDom)
     // console.dir(render(virtualDom))
 
     // console.dir(container)
