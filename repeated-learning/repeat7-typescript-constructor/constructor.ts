a16fefeb7878c3ca0be4c5556cd911890a83f315
HongMoonGI <hmoongi94@gmail.com>
2023-12-07 12:40:59 +0900

    question: static메서드는 new로 인스턴스를 생성하지 않고도 사용할 수 있다?

--- repeated-learning/repeat7-typescript-constructor/constructor.ts
+++ repeated-learning/repeat7-typescript-constructor/constructor.ts
@@ -74,3 +74,13 @@ const calc = new Calculator(10,5)
 console.log(Calculator.addStatic(20,5))
 console.log(calc)
 console.log(calc.add())
+
+/**
+ * 생성자 함수의 구조를 위의 console.dir()로 확인하면 아래의 '구조'를 확인할 수 있습니다.
+ * static, 즉 정적 메서드는 생성자 함수의 프로퍼티로 붙어있는 것이 아니라, 생성자 함수 자체의 프로퍼티로 붙어있는 것을 확인할 수 있습니다.
+ * static(정적이다)라는 해당 표현의 의미와 public 키워드의 차이는
+ *? 정적 메서드는 생성자 함수의 인스턴스를 생성하지 않고도 사용할 수 있지만, public메서드는 인스턴스를 생성해야만 사용할 수 있다는 것입니다.
+ *? 인스턴스를 생성하지 않으면 static메서드가 사용이 안되는거 같은데
+ * 우리가 흔히 사용하고 봐왔던 메서드는 public 메서드입니다.
+ *? static메서드와 매우 혼동되는 부분이므로 마지막 Calcultator.addStatic()메서드는 new키워드를 사용하지 않고서도 사용할 수 있습니다. ->이 부분 해봤는데 안됐었음
+ */
