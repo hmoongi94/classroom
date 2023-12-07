a55cb1211215c441fd3b4135496e1bff212f8de6
HongMoonGI <hmoongi94@gmail.com>
2023-12-07 10:34:31 +0900

    study: 얕은 복사, 깊은 복사의 개념 이해하기

--- typescript/typescript-4/index.ts
+++ typescript/typescript-4/index.ts
@@ -4,17 +4,33 @@
  * User라는 인터페이스를 따르는 객체는 id와 name 속성을 가져야 하고, id는 number타입, name은 string타입이어야만 하게끔
  * 타입 안정성(tpye safety)을 보장합니다.
  */
-
-interface User{
+interface User {
   id: number;
   name: string;
 }
-/** 
+
+/**
  * users라는 배열은 인터페이스(특정 형식)인 User를 따르는 객체만을 원소로 가질 수 있게끔 제한됩니다.
  * 실제적인 코드로서는 위 interface만 확인하면 어떤 객체든 무슨 내용이 어떤 구조로 되어있는지 추론 할 수 있게됩니다.
  */
+const users: User[] = [
+  { id: 1, name: "홍문기" },
+  { id: 2, name: "홍현기" },
+];
 
-const users: User[] =[ 
-  {id:1, name: "홍문기"},
-  {id:2, name: "홍현기"}
-]
+/**
+ * @param obj User 객체
+ * @returns Object를 JSON 문자열로 변환한 후
+ */
+function jsonConvertObject(obj: User): User {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+// 얕은 복사
+/**
+ * 얕은 복사의 개념은 배열의 원소가 객체일 경우, 원소의 객체를 복사하지 않고 참조만 복사하는 것을 의미합니다.
+ * 아래의 전개 연산자(spread operator)를 통해 객체를 복사하고 있지만, 객체의 속성이 객체일 경우, 참조만 복사하게 됩니다.
+ */
+const shallowCopiedUsers: User[] = users.map((user) => {
+  return user;
+});
