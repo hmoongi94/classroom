74b95f0ceb6e7a1b78254adbcc480df80a266d92
HongMoonGI <hmoongi94@gmail.com>
2023-12-14 17:13:42 +0900

    feat: 인스턴스의 메서드 사용예시? static메서드의 기능?

--- repeated-learning/repeat9-functionProgramming/functionProgramming.js
+++ repeated-learning/repeat9-functionProgramming/functionProgramming.js
@@ -19,3 +19,7 @@ console.log(sayHelloMinam()) //"hello hongmoongi" 출력
 // Chunam 팩토리 함수를 사용하여 객체 생성 및 메서드 사용 예시
 const chunamInstance = createChunam()
 console.log(createChunam())
+// class가 아니므로 new라는 키워드를사용하진 않앗지만
+// 사실상 함수를 호출하자마자 객체가 생성되는 것이므로
+// class와 비슷한 방식
+console.log(chunamInstance.sayHello())
