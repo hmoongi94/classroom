8dce972cc6151135612c7b304c34e422cbc96f27
HongMoonGI <hmoongi94@gmail.com>
2023-12-14 17:21:00 +0900

    feat: static메서드를 사용하는 이유를 알려주는 예제

---
+++ repeated-learning/repeat9-class/staticCompareWithoutstatic.js
@@ -0,0 +1,23 @@
+class Minam{
+  static sayHello(){
+    return "hello hongmoongi"
+  }
+}
+
+class Chunam{
+  sayHello(){
+    return "bye hongmoongi"
+  }
+}
+
+// Minam 클래스의 static 메서드 사용 예시
+console.log(Minam.sayHello()) // hello hongmoongi
+
+// Chunam 클래스의 인스턴스 생성 및 메서드 사용 예시
+// new라는 키워드를 통해서 chunamInstance라는 인스턴스(객체)를 생성
+// chunamInstance 인스턴스에는 sayHello 메서드가 있으므로 사용이 가능하다.
+// 하지만 위 Minam 클래스의 sayHello는 new 키워드 없는, 즉 인스턴스를 따로 생성하지 않아도 사용이 가능하다.
+// 일반적인 인스턴스 생성은 다분히 동적인 방식이라고 할 수 있다.
+console.log(chunamInstance.sayHello())
+const chunamInstance = new Chunam();
+// console.log(chunamInstance.sayHello())
