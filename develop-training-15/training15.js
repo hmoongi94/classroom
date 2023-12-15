13a2a683ab3bfab2035d2d820789b864a85ed9bd
HongMoonGI <hmoongi94@gmail.com>
2023-12-15 17:03:41 +0900

    feat: training15예제 React 작성 패턴과, 내부 논리구조, 그리고 비동기 처리, 컨테이너(여기서는 컴포넌트) 래핑등 종합적인 패턴들이 작성되어있습니다.

---
+++ develop-training-15/training15.js
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react'
+
+const fetchProducts = async()=>{
+  return new Promise(resolve=>{
+    setTimeout(()=>{
+      resolve([
+        {id:1, title: '김치', price: '5000원'},
+        {id:2, title: '라면', price: '3000원'},
+        {id:3, title: '삼겹살', price: '15000원'},
+      ])
+    }, 500)
+  })
+}
+
+const Carousel = ({fetchProducts})=>{
+  const [products, setProducts] = useState([])
+
+  useEffect(()=>{
+    const loadProducts = async () =>{
+      const data = await fetchProducts()
+      setProducts(data)
+    }
+    loadProducts()
+  }, [fetchProducts])
+
+  if(products.length === 0 ){
+    return <div>Loading...</div>
+  }
+
+  return(
+    <div>
+      <ul>
+        {products.map((product)=> (
+          <li key={product.id}>
+            {product.title} - {product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+// export default Carousel
+
+const Example = ()=>{
+  return <Carousel fetchProducts = {fetchProducts} />
+}
+
+// export default Example
