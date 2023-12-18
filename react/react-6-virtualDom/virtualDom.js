956b5fd32755a70a24522bc21e355e2920688dfb
HongMoonGI <hmoongi94@gmail.com>
2023-12-18 16:39:32 +0900

    feat: children객체 처리. 재귀함수?

--- react/react-6-virtualDom/virtualDom.js
+++ react/react-6-virtualDom/virtualDom.js
@@ -9,13 +9,27 @@ export const load = () => {
      * 추후 Typescript로 말끔히 해결할 수 있습니다.
      * 
      * @param {string} type - 생성할 요소의 타입 (예: 'div', 'h1')
+     * @param {Object} properties - 요소의 속성 (예: { id: 'app'})
+     * @param {Array} children - 자식 요소들(문자열 또는 객체)
+     * @returns {Object} 생성된 가상 DOM 요소
      */
     function createElementExample(type, properties, children){
+      // 자식 요소들을 가상 Dom 요소로 반환
       const props = {
         children: children.map(child=>{
-
+          // children 요소가 객체일때는 리턴하고, 아니면 다시 본 함수 호출(재귀함수)
+          // 객체가 아닌경우 텍스트를 가진 가상 DOM 요소를 생성
+          // 어려운 코드 테크닉이므로, 태그가 아닌 기본 형태를 가지고 연습하시기 바랍니다.
+          if(typeof child==='object'){
+            return child;
+          } else{
+            return createTextElementExample(child)
+          }
         })
       }
+
+      // 속성을 복사
+      
     }
   })
 }
