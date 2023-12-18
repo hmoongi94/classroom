c7045b6f689ba0ef40d8961254478e26dda45a4b
HongMoonGI <hmoongi94@gmail.com>
2023-12-18 19:18:30 +0900

    feat: 렌더링 해보기

--- react/react-7-Arraymethod/reactVersion.js
+++ react/react-7-Arraymethod/reactVersion.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const PokemonList = () => {
-  const pokemon = ['피카츄', '라이츄', '파이리', '꼬부기']
+  const pokemon = ['피카츄', '라이츄', '파이리', '꼬부기'];
 
   const updatedPokemon = pokemon.map((pokemonName) => {
     if (pokemonName === '파이리') {
-      return '버터풀'
+      return '버터풀';
     }
-    return pokemonName
-  })
+    return pokemonName;
+  });
 
-  const pokemonElements = updatedPokemon.map((pokemonName,
+  const pokemonElements = updatedPokemon.map((pokemonName, index) => (
     <div key={index}>
       <p>{pokemonName}</p>
     </div>
-  ))
+  ));
 
-  return <div>{pokemonElements}</div>
-}
+  return <div>{pokemonElements}</div>;
+};
 
-export default PokemonList
+export default PokemonList;
