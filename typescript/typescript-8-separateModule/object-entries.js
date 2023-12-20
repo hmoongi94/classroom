d346559a5170e8eea07343ddc393f632cbc934c1
HongMoonGI <hmoongi94@gmail.com>
2023-12-20 18:24:34 +0900

    feat: for..in/for..of 이해하기

--- typescript/typescript-8-separateModule/object-entries.js
+++ typescript/typescript-8-separateModule/object-entries.js
@@ -1,4 +1,4 @@
-//* Object.entries 이해하기
+// * Object.entries 이해하기
 const person = {
   name: 'John',
   age: 30,
@@ -9,8 +9,21 @@ const entries = Object.entries(person);
 
 // console.log(entries);
 
-for(let [key,value] of entries){
-  console.log(key,value)
-}
 
-//* for of/for in 이해하기
+// * for of/for in 이해하기
+// for(let [key,value] of entries){
+//   console.log(key,value)
+// }
+
+const objKey = { a: 1, b: 2, c: 3 };
+
+// for (let key in objKey) {
+//   console.log(key, objKey[key]);
+// }
+
+// * value라고 변수지정한다고 value가 나오는 것이 아님 
+// * 위의 let[key,value]를 사용하거나 key,변수[key]로 값을 가져와야한다?
+// ? 객체[key]를 사용하는 것이아니면 바로 value값은 불러올 수 없나?
+for (let value in objKey) {
+  console.log(value);
+}
