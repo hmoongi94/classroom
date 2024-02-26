5c822998db66e0203ca0eb4a5666f38d5b29e448
HongMoonGI <hmoongi94@gmail.com>
2024-02-26 09:36:26 +0900

    feat: 파이썬 crud연습해보기

---
+++ repeated-learning/repeat16-python-crud/python.py
@@ -0,0 +1,18 @@
+# json 임포트
+# json을 사용하려면 node.js와 다르게 임포트 해야한다.
+import json
+
+# 예제를 위한 임의의 json파일 경로
+file_path = 'example.json'
+
+# 개발자가 만들 함수
+def update_func(data):
+  # 내용
+  return
+
+def delete_func(data):
+  # 내용
+  return
+
+def create_json(data):
+  # 
